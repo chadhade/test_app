4f3cc8a51547d32ad7aa75ce92bc08c534d4a5fa
Deepal Chadha <deepal.chadha@gmail.com>
2011-09-13 16:31:54 -0400

    initial commit: empty app immediately running rails new thought_log

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThoughtLog::Application.config.secret_token = '99b46b719e122fff3037660fe8444d5e2b27c24f4b2f37b5d3cbf84507408e7848c7377ca43a4f958abaec0fd63eeac87ee58ae5a1ccae05c8765bdcbf291c24'
