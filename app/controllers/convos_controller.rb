0f123554dc8ca18e2be2f299edd6e87d7d423aee
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-10 17:50:03 -0400

    created user page to create a new convo

--- app/controllers/convos_controller.rb
+++ app/controllers/convos_controller.rb
@@ -1,8 +1,15 @@
 class ConvosController < ApplicationController
   def new
+    @convo = Convo.new
   end
 
   def create
+	@convo = Convo.new(params[:convo])
+	if @convo.save
+	  redirect_to @convo
+	else
+	  render 'new'
+	end
   end
 
   def show
