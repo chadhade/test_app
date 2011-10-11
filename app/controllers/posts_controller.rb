c59a60dbc251f3ff4b81bc6c823b8988e6e1a103
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-11 18:14:32 -0400

    set current user upon first response

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,12 @@ class PostsController < ApplicationController
 	current_user.posts.create(params[:post])
 	redirect_to convo_path(@convo_id)
 	
-	#create a new entry in ConvoUser the first time joining a conversation and sets current_turn
+    #create a new entry in ConvoUser the first time joining a conversation and sets current_turn
 	if ConvoUser.find_by_user_id_and_convo_id(current_user.id, @convo_id).nil?
 	  ConvoUser.new(:user_id => current_user.id, :convo_id => @convo_id, :creator => false).save
-	  # current_turn = creator of convo
+	  # set current turn
+	  @creator_id = ConvoUser.find_by_convo_id_and_creator(@convo_id, true).user_id
+	  Convo.find_by_id(@convo_id).update_attributes(:current_turn => @creator_id)
 	end
   end
 
