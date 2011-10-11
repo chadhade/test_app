ca5ee15e1e158beccaba6e57c6a5c5782dcfc4e5
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-11 17:54:14 -0400

    when you join a convo, ConvoUser database updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,11 @@ class PostsController < ApplicationController
   def create
     current_user.posts.create(params[:post])
 	redirect_to convo_path(params[:post][:convo_id])
+	
+	#create a new entry in ConvoUser the first time joining a conversation
+	if ConvoUser.find_by_user_id_and_convo_id(current_user.id, params[:post][:convo_id]).nil?
+	  ConvoUser.new(:user_id => current_user.id, :convo_id => params[:post][:convo_id], :creator => false).save
+	end
   end
 
 end
