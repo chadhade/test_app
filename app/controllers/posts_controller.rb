39f2514ededca2ac5d33a52fb14524beae946195
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-11 18:01:43 -0400

    refactored code in posts controller a bit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    current_user.posts.create(params[:post])
-	redirect_to convo_path(params[:post][:convo_id])
+    @convo_id = params[:post][:convo_id]
 	
-	#create a new entry in ConvoUser the first time joining a conversation
-	if ConvoUser.find_by_user_id_and_convo_id(current_user.id, params[:post][:convo_id]).nil?
-	  ConvoUser.new(:user_id => current_user.id, :convo_id => params[:post][:convo_id], :creator => false).save
+	current_user.posts.create(params[:post])
+	redirect_to convo_path(@convo_id)
+	
+	#create a new entry in ConvoUser the first time joining a conversation and sets current_turn
+	if ConvoUser.find_by_user_id_and_convo_id(current_user.id, @convo_id).nil?
+	  ConvoUser.new(:user_id => current_user.id, :convo_id => @convo_id, :creator => false).save
+	  # current_turn = creator of convo
 	end
   end
 
