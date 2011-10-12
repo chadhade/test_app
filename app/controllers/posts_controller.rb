3dda82587c2fa727f82566fe45231ade92ffe6c5
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-11 21:44:05 -0400

    included starttime for convos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,14 +11,14 @@ class PostsController < ApplicationController
   def create
     @convo_id = params[:post][:convo_id]
 	@convo = Convo.find_by_id(@convo_id)
-	current_user.posts.create(params[:post])
+	@post = current_user.posts.create(params[:post])
 	
     #create a new entry in ConvoUser the first time joining a conversation and sets current_turn
 	if ConvoUser.find_by_user_id_and_convo_id(current_user.id, @convo_id).nil?
 	  ConvoUser.new(:user_id => current_user.id, :convo_id => @convo_id, :creator => false).save
 	  # set current turn
 	  @creator_id = ConvoUser.find_by_convo_id_and_creator(@convo_id, true).user_id
-	  @convo.update_attributes(:current_turn => @creator_id)
+	  @convo.update_attributes(:current_turn => @creator_id, :start_time => @post.created_at)
 	#otherwise update current_turn
 	else
 	  # current_turn = !current_user
